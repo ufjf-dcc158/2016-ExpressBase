c73e89313198e196fdeb5f658ec8dd7521b91b3d
Igor Knop <igorknop@gmail.com>
2016-12-03 21:53:01 -0200

    Adiciona mais operações do CRUD

--- app/controllers/user.controllers.server.js
+++ app/controllers/user.controllers.server.js
@@ -9,5 +9,61 @@ module.exports.create = function(req, res,
        res.json(user);
      }
    });
+}
 
+
+module.exports.list = function(req, res, next){
+  User.find({}, function(err, users) {
+    if(err){
+      next(err);
+    } else {
+      res.json(users);
+    }
+  });
+}
+
+module.exports.getById = function(req, res, next, id){
+  User.findOne({"_id":id}, function(err, user){
+    if(err){
+      res.json({});
+    }else{
+      req.user = user;
+      next();
+    }
+  });
+}
+
+module.exports.read = function(req, res, next){
+  res.json(req.user);
+}
+
+
+module.exports.update = function(req, res, next){
+  User.findByIdAndUpdate(
+    req.user.id,
+    req.body,
+    function(err, user2){
+      if(err){
+        next(err);
+      }else{
+        res.json(user2);
+      }
+    }
+  );
+
+}
+
+
+module.exports.remove = function(req, res, next){
+  User.findByIdAndRemove(
+    req.user.id,
+    req.body,
+    function(err, user2){
+      if(err){
+        next(err);
+      }else{
+        res.json(user2);
+      }
+    }
+  );
 }
